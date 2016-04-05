8707e14b4bdeca7bcc8702e01ab68f4ea2761c11
Damelys <mely.rq@gmail.com>
2016-04-05 16:37:56 -0430

    Modificación en formato de la etiqueta

--- app/pdfs/etiqueta_pdf.rb
+++ app/pdfs/etiqueta_pdf.rb
@@ -1,23 +1,30 @@
- #encoding: UTF-8
-class EtiquetaPdf < Prawn::Document	
-	
-	def initialize(empresa, telefono)
-		super(:page_layout => :portrait, :page_size => [355, 185])
+#encoding: UTF-8
+class EtiquetaPdf < Prawn::Document
 
-		font("#{Rails.root}/fonts/arial.ttf", :size => 8) do
+  def initialize(empresa, telefono)
+    super(:page_layout => :portrait,:margin => [5, 3], :page_size => [235, 110])
 
-			draw_text "#{empresa.nombre_empresa.strip}", :at  => [-25,135]
-			draw_text "Contacto #{empresa.rep_ean.strip.upcase}", :at => [-25,127]
-			draw_text "Cargo #{empresa.rep_ean_cargo.strip}", :at => [-25,119]
-			draw_text "#{empresa.direccion_ean.strip}", :at => [-25,111]
-			draw_text "Pto Ref #{empresa.try(:punto_ref_ean)}", :at => [-25,103]
-			draw_text "Estado #{empresa.try(:estado).try(:nombre)}  Ciudad #{empresa.try(:ciudad).try(:nombre).upcase}  Parroquia #{empresa.try(:parroquia_ean)}" , :at => [-25,95]
-			draw_text "Municipio #{empresa.try(:municipio).try(:nombre)}                                Prefijo #{empresa.prefijo}", :at => [-25,87]
-			draw_text "Cod Postal #{empresa.try(:cod_postal_ean).strip}                      Telefono #{telefono} ", :at => [-25,79]
-			
-			
-		end
-		
-	end
+    font("#{Rails.root}/fonts/arial.ttf", :size => 8) do
+
+      draw_text "#{empresa.nombre_empresa.strip}", :at  => [0,85]
+      draw_text "Contacto: #{empresa.rep_ean.strip.upcase}", :at => [0,75]
+      draw_text "Cargo: #{empresa.rep_ean_cargo.strip}", :at => [0,67]
+      draw_text "Dirección:", :at => [0,59]
+      text_box("#{empresa.direccion_ean.strip}", options={:at => [40,65], :width  =>200})
+      draw_text "Pto Ref: #{empresa.try(:punto_ref_ean)}", :at => [0,37]
+      draw_text "Estado: #{empresa.try(:estado).try(:nombre)}", :at => [0,29]
+      draw_text "Ciudad: #{empresa.try(:ciudad).try(:nombre).upcase}", :at => [60,29]
+      draw_text "Municipio: #{empresa.try(:municipio).try(:nombre)}", :at => [0,21]
+      draw_text "Parroquia: #{empresa.try(:parroquia_ean)}", :at => [60,21]
+      draw_text "C. Postal: #{empresa.try(:cod_postal_ean).strip}", :at => [0,13]
+      draw_text "Código: #{empresa.prefijo}", :at => [60,13]
+      draw_text "Teléfonos #{telefono}", :at => [0,5]
+
+
+
+
+    end
+
+  end
 
 end
