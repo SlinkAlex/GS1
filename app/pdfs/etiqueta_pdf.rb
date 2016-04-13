8618ad6e5dbdbdd1d3cfdf442a93e9deb0734cf0
Damelys <mely.rq@gmail.com>
2016-04-13 17:01:16 -0430

    Se modifica para poder imprimir el municipio en la etiqueta

--- app/pdfs/etiqueta_pdf.rb
+++ app/pdfs/etiqueta_pdf.rb
@@ -10,7 +10,7 @@ class EtiquetaPdf < Prawn::Document
       draw_text "Contacto: #{empresa.rep_ean.strip.upcase}", :at => [0,90]
       draw_text "Cargo: #{empresa.rep_ean_cargo.strip}", :at => [0,80]
       draw_text "Dirección:", :at => [0,70]
-      text_box("#{empresa.direccion_ean.strip}", options={:at => [40,75], :width  =>230})
+      text_box("#{empresa.direccion_ean.strip}", options={:at => [40,75], :width  =>225})
       draw_text "Pto Ref: #{empresa.try(:punto_ref_ean)}", :at => [0,41]
       draw_text "Estado: #{empresa.try(:estado).try(:nombre)}", :at => [0,31]
       draw_text "Ciudad: #{empresa.try(:ciudad).try(:nombre).upcase}", :at => [100,31]
