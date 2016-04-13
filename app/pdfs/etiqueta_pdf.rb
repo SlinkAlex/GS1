0dcd60596acb4b9d0f1e928ab6d4278e88f674cb
Damelys <mely.rq@gmail.com>
2016-04-13 16:22:12 -0430

    Se modifica para poder imprimir el municipio en la etiqueta

--- app/pdfs/etiqueta_pdf.rb
+++ app/pdfs/etiqueta_pdf.rb
@@ -1,9 +1,9 @@
 #encoding: UTF-8
 class EtiquetaPdf < Prawn::Document
 
-  def initialize(empresa, telefono)
-    super(:page_layout => :portrait,:margin => [0, 10], :page_size => [275, 130])
+  def initialize(empresa, telefono, municipio_ean)
 
+    super(:page_layout => :portrait,:margin => [0, 10], :page_size => [275, 130])
     font("#{Rails.root}/fonts/arial.ttf", :size => 8) do
 
       draw_text "#{empresa.nombre_empresa.strip}", :at  => [0,100]
@@ -14,14 +14,11 @@ class EtiquetaPdf < Prawn::Document
       draw_text "Pto Ref: #{empresa.try(:punto_ref_ean)}", :at => [0,41]
       draw_text "Estado: #{empresa.try(:estado).try(:nombre)}", :at => [0,31]
       draw_text "Ciudad: #{empresa.try(:ciudad).try(:nombre).upcase}", :at => [100,31]
-      draw_text "Municipio: #{empresa.try(:municipio).try(:nombre)}", :at => [0,21]
+      draw_text "Municipio: #{municipio_ean.try(:nombre)}", :at => [0,21]
       draw_text "Parroquia: #{empresa.try(:parroquia_ean)}", :at => [100,21]
       draw_text "C. Postal: #{empresa.try(:cod_postal_ean).strip}", :at => [0,11]
       draw_text "Código: #{empresa.prefijo}", :at => [100,11]
       draw_text "Teléfonos #{telefono}", :at => [0,1]
-
-
-
 
     end
 
