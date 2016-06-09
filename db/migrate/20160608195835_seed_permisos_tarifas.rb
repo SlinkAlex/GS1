67d07039893b24616903a8d64f6ae14e5fde42b7
Damelys <mely.rq@gmail.com>
2016-06-09 13:08:13 -0430

    Modificación referente a tarifas vigentes.

--- db/migrate/20160608195835_seed_permisos_tarifas.rb
+++ db/migrate/20160608195835_seed_permisos_tarifas.rb
@@ -4,6 +4,7 @@ class SeedPermisosTarifas < ActiveRecord::Migration
     execute "INSERT INTO usuarios_alcances ([perfil],[alcance],[gerencia],[created_at],[updated_at]) VALUES ('Gerente','Crear Tarifas','Comercial','2016-06-08 15:44:54.170','2016-06-08 15:44:54.170')"
     execute "INSERT INTO usuarios_alcances ([perfil],[alcance],[gerencia],[created_at],[updated_at]) VALUES ('Gerente','Editar Tarifas','Comercial','2016-06-08 15:44:54.170','2016-06-08 15:44:54.170')"
     execute "INSERT INTO usuarios_alcances ([perfil],[alcance],[gerencia],[created_at],[updated_at]) VALUES ('Gerente','Eliminar Tarifas','Comercial','2016-06-08 15:44:54.170','2016-06-08 15:44:54.170')"
+
   end
   def down
   end
