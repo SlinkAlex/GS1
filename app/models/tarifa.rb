947de3f2f66f31732ae96b757ee0d75e2af38834
Damelys <mely.rq@gmail.com>
2016-06-09 11:52:14 -0430

    Modificación referente a tarifas vigentes

--- app/models/tarifa.rb
+++ app/models/tarifa.rb
@@ -1,11 +1,17 @@
 class Tarifa < ActiveRecord::Base
   self.table_name = "tarifas"
-  attr_accessible :desde,:hasta, :aporte_mantenimiento
+  attr_accessible :desde,:hasta, :aporte_bs, :tipo_aporte, :id_tipo_usuario
+  belongs_to :tipo_usuario_empresa, :foreign_key =>  "id_tipo_usuario"
+
+  validates_numericality_of :desde, :on => :create , :message =>" - Es un campo numerico"
+  validates_numericality_of :hasta, :on => :create , :message =>" - Es un campo numerico"
+  validates_numericality_of :aporte_bs, :on => :create , :message =>" - Debe ingresar un monto"
 
   validate do |tarifa|
     tarifa.errors[:base] <<("Campo DESDE es requerido") if tarifa.desde.nil?
     tarifa.errors[:base] <<("Campo HASTA es requerido") if  tarifa.hasta.nil?
-    tarifa.errors[:base] <<("Campo APORTE MANTENIMIENTO esrequerido") if tarifa.aporte_bs.nil?
+    tarifa.errors[:base] <<("Campo APORTE DE MANTENIMIENTO es requerido") if  tarifa.aporte_bs.nil?
+    tarifa.errors[:base] <<("Campo TIPO DE USUARIO es requerido") if  tarifa.id_tipo_usuario.nil?
 
   end
 
