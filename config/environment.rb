ed2dd6194eeacc0f2f4a4a3a50bf5bb5c995a49b
Alexander Delgado <alexander.delgado@segurossura.com.ar>
2021-10-18 13:36:44 -0400

    fix error

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 GS1::Application.initialize!
-
-Rails.logger = Logger.new(STDOUT)
-config.logger = ActiveSupport::Logger.new("log/#{Rails.env}.log")
